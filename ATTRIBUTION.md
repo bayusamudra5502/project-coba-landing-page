aad08d354c856c91351c8aa8359041b23a1d8ad2
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-06-14 23:52:03 +0700

    Menambah beberapa file dan mengalihkan css ke scss

---
+++ ATTRIBUTION.md
@@ -0,0 +1,6 @@
+# Atribusi Aset Gambar
+Berikut ini adalah sumber aset yang digunakan pada proyek ini:
+
+1. Undraw (https://undraw.co/) - Lisensi : https://undraw.co/license
+2. Feather (https://feathericons.com/) - Lisensi : MIT
+3. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 // Header
 const menuButton = document.querySelector("#menu");
-menuButton.addEventListener("click", () => {
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
     const nav = document.querySelector("header nav");
     const menuImg = document.querySelector("#menu-img");
     const closeImg = document.querySelector("#close-img");
     nav.classList.toggle("show");
     menuImg.classList.toggle("hide");
     closeImg.classList.toggle("hide");
-});
+}
