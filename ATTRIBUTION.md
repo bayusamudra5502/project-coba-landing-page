6aa60924744cd66cf9f927e9328d4635fea86ea1
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-06-20 22:31:31 +0700

    Menyelesaikan sampai qna

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -3,4 +3,13 @@ Berikut ini adalah sumber aset yang digunakan pada proyek ini:
 
 1. Undraw (https://undraw.co/) - Lisensi : https://undraw.co/license
 2. Feather (https://feathericons.com/) - Lisensi : MIT
-3. 
+3. Unsplash - Beberapa foto berikut:
+* [Ilustrasi Wireframe Oleh HalGatewood.com](https://unsplash.com/photos/k-rKfqSm4L4)
+* [Foto Macbook dan Code Editor Oleh Nathan da Silva](https://unsplash.com/photos/tZc3vjPCk-Q)
+* [Ilustrasi Kode Javascript React oleh Juanjo Jaramillo](https://unsplash.com/photos/mZnx9429i94)
+* [Ilustrasi Desain Web oleh Eftakher Alam](https://unsplash.com/photos/i1VQZsU86ok)
+* [Ilustrasi Laptop oleh Ben Kolde](https://unsplash.com/photos/bs2Ba7t69mM)
+* [Ilustrasi Cat oleh David Pisnoy](https://unsplash.com/photos/46juD4zY1XA)
+* [Gambar Seorang Lelaki oleh bruce mars](https://unsplash.com/photos/8YG31Xn4dSw)
+* [Gambar oleh Linkedln Sales Solutions](https://unsplash.com/photos/pAtA8xe_iVM)
+* [Gambar oleh Austin Distel](https://unsplash.com/photos/7uoMmzPd2JA)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,104 @@
-// Header
 const menuButton = document.querySelector("#menu");
-menuButton.addEventListener("click", toggleMenu);
+const menuLinks = document.querySelectorAll("header ul li");
+const featureItem = document.querySelectorAll(".feature-item");
+const kelas = document.querySelector("#kelas")
+const leftSliderButton = document.querySelector(".left-button")
+const rightSliderButton = document.querySelector(".right-button")
+const cardContainer = document.querySelector(".card-container")
+const daftarPertanyaan = document.querySelectorAll(".set-pertanyaan")
 
 function toggleMenu() {
-    const nav = document.querySelector("header nav");
-    const menuImg = document.querySelector("#menu-img");
-    const closeImg = document.querySelector("#close-img");
-    nav.classList.toggle("show");
-    menuImg.classList.toggle("hide");
-    closeImg.classList.toggle("hide");
+    if (screen.width <= 600) {
+        const nav = document.querySelector("header nav");
+        const menuImg = document.querySelector("#menu-img");
+        const closeImg = document.querySelector("#close-img");
+        nav.classList.toggle("show");
+        menuImg.classList.toggle("hide");
+        closeImg.classList.toggle("hide");
+    }
 }
+
+// Attach Listener
+
+menuButton.addEventListener("click", toggleMenu);
+
+menuLinks.forEach((el) => {
+    el.addEventListener("click", toggleMenu);
+});
+
+document.addEventListener("scroll", () => {
+    const Y_AXIS = window.scrollY;
+    const bodyOffset = document.body.getBoundingClientRect().top;
+
+    featureItem.forEach((el) => {
+        const deviceOffset = screen.width <= 600 ? 1 : 2.5
+        const elBoundCliRect = el.getBoundingClientRect()
+        const ypos = elBoundCliRect.top - bodyOffset - elBoundCliRect.height * deviceOffset;
+
+        if (Y_AXIS >= ypos && !el.classList.contains("show")) {
+            el.classList.add("show");
+        } else if (Y_AXIS < ypos && el.classList.contains("show")) {
+            el.classList.remove("show");
+        }
+    });
+});
+
+leftSliderButton.addEventListener("click", () => cardSlider(false))
+rightSliderButton.addEventListener("click", () => cardSlider())
+
+let cardPosition = 0;
+function cardSlider(toRight = true) {
+    const cardWidth = elementWidth(document.querySelector("#kelas .card"));
+    const cardContainerWidth = elementWidth(cardContainer);
+
+    if (toRight)
+        cardPosition = Math.max(cardPosition - cardWidth - 40, screen.width - cardContainerWidth);
+    else
+        cardPosition = Math.min(cardPosition + cardWidth + 40, 0);
+
+    cardContainer.style.transform = `translateX(${cardPosition}px)`
+}
+
+window.addEventListener("load", () => {
+    const testimoniSlider = document.querySelector("#testimoni .slider");
+    
+    let sliderPos = 0;
+    let isLast = false;
+    
+    setInterval(() => {
+        const cardWidth = elementWidth(testimoniSlider.children[0]) + 10;
+        const batas = screen.width - elementWidth(testimoniSlider) - 10;
+
+        sliderPos -= cardWidth;
+
+        if (isLast) {
+            sliderPos = 0;
+            isLast = false;
+        }
+
+        if (batas > sliderPos) {
+            sliderPos = batas;
+            isLast = true;
+        }
+
+        testimoniSlider.style.transform = `translateX(${sliderPos}px)`;
+    }, 5000)
+})
+
+function elementWidth(el) {
+    return el.getBoundingClientRect().width
+}
+
+daftarPertanyaan.forEach((el) => {
+    const jawaban = el.querySelector(".jawaban")
+    el.addEventListener("click", function(){
+        this.classList.toggle("show")
+        const contentHeight = jawaban.scrollHeight;
+
+        if(this.classList.contains("show")){
+            jawaban.style.maxHeight = `${contentHeight}px`;
+        }else{
+            jawaban.style.maxHeight = "0px";
+        }
+    })
+})
